794ee6f9ce237246caa7089de31aa98d2a2e1f61
Connor O'Leary <elimzke@gmail.com>
2024-07-22 00:23:50 +1000

    TCS listen and stop func, build_release_editor updated for dev

--- build_release_editor.bat
+++ build_release_editor.bat
@@ -1,1 +1,17 @@
 scons -j24 platform=windows bits=64 target=release_debug
+
+cd bin
+
+C:\msys64\ucrt64\bin\strip.exe godot.windows.opt.tools.64.exe
+
+del Godot.exe
+
+rename godot.windows.opt.tools.64.exe Godot.exe
+
+del *.exp
+
+del *.lib
+
+del *.pdb
+
+cd ..
--- modules/turnt/netcode/turnt_chat_server.h
+++ modules/turnt/netcode/turnt_chat_server.h
@@ -2,6 +2,7 @@
 #define TURNT_CHAT_SERVER
 
 #include "core/io/tcp_server.h"
+#include "core/os/os.h"
 #include "turnt_globals.h"
 
 #include "tnt_macros.h"
@@ -10,18 +11,60 @@
 #endif // TNT_CLASS_NAME
 #define TNT_CLASS_NAME TurntChatServer
 
+// TODO: Replace with C++ version of TntLogger once ready
+#define TNT_LOG(msg, ...) OS::get_singleton()->print("[ChatServer] " msg "\n", __VA_ARGS__)
+#define TNT_LOGERR(msg, ...) OS::get_singleton()->printerr("[ChatServer] " msg "\n", __VA_ARGS__)
+
+static int DEFAULT_NUM_MAX_CONNECTIONS = 64;
+static int DEFAULT_PORT = 25235;
+
 class TurntChatServer : public TCP_Server
 {
     GDCLASS(TurntChatServer, TCP_Server);
 
 public:
-    TurntChatServer() = default;
+    TurntChatServer()
+        : m_num_max_connections(DEFAULT_NUM_MAX_CONNECTIONS)
+        , m_tcp_port(DEFAULT_PORT)
+    { }
 
-    bool start_server() { return false; }
-    void stop_server() { }
+    bool start_server() 
+    { 
+        if (is_listening())
+        {
+            TNT_LOG("start_server() called when already listening!");
+            return true; // Return true since if we're already listening, then we technically HAVE successfully started the server..
+        }
 
+        Error err = listen(static_cast<uint16_t>(m_tcp_port));
+        if (err != Error::OK)
+        {
+            TNT_LOGERR("Error listening on port %d with err code %d", m_tcp_port, err);
+            return false;
+        }
+        else
+        {
+            TNT_LOG("Started listening on port %d", m_tcp_port);
+            return true;
+        }
+    }
+
+    void stop_server() 
+    { 
+        if (!is_listening())
+        {
+            TNT_LOG("stop_server() called when not listening!");
+            return;
+        }
+
+        stop();
+        TNT_LOG("Stopped listening. Connections will now be closed.");
+        // TODO: Loop through connections and kick em awf, kid
+    }
+
+public:
     TNTPROP_GS(int, num_max_connections);
-    TNTPROP_G(bool, is_online);
+    TNTPROP_GS(int, tcp_port);
 
 private:
     void handle_new_connection() { }
@@ -33,7 +76,7 @@ protected:
         ClassDB::bind_method(D_METHOD("stop_server"), &TurntChatServer::start_server);    
 
         TNTADD_GS(INT, num_max_connections);
-        TNTADD_G(BOOL, is_online);
+        TNTADD_GS(INT, tcp_port);
     }
 };
 
